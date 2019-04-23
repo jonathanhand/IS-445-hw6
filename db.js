c0c4e6f73267b7e886a7f3f088402c390cc8cb22
jonathanhand <hand.jonathan.13@gmail.com>
2019-04-23 12:15:26 -0700

    prettier

--- db.js
+++ db.js
@@ -1,76 +1,76 @@
-let datastore
+let datastore;
 
 /**
  * Open a connection to the datastore.
  */
 export function openDb(callback) {
   // Database version.
-  const version = 1
+  const version = 1;
 
   // Open a connection to the datastore.
-  const request = indexedDB.open('todos', version)
+  const request = indexedDB.open("todos", version);
 
   // Handle datastore upgrades.
   request.onupgradeneeded = function(e) {
-    const db = e.target.result
+    const db = e.target.result;
 
-    e.target.transaction.onerror = onDberror
+    e.target.transaction.onerror = onDberror;
 
     // Delete the old datastore.
-    if (db.objectStoreNames.contains('todo')) {
-      db.deleteObjectStore('todo')
+    if (db.objectStoreNames.contains("todo")) {
+      db.deleteObjectStore("todo");
     }
 
     // Create a new datastore.
-    db.createObjectStore('todo', {
-      keyPath: 'timestamp',
-    })
-  }
+    db.createObjectStore("todo", {
+      keyPath: "timestamp"
+    });
+  };
 
   // Handle successful datastore access.
   request.onsuccess = function(e) {
     // Get a reference to the DB.
-    datastore = e.target.result
+    datastore = e.target.result;
 
     // Execute the callback.
-    callback()
-  }
+    callback();
+  };
 
   // Handle errors when opening the datastore.
-  request.onerror = onDberror
+  request.onerror = onDberror;
 }
 
 /**
  * Fetch all of the todo items in the datastore.
  */
 export function fetchTodos(callback) {
-  const db = datastore
-  const transaction = db.transaction(['todo'], 'readwrite')
-  const objStore = transaction.objectStore('todo')
+  const db = datastore;
+  const transaction = db.transaction(["todo"], "readwrite");
+  const objStore = transaction.objectStore("todo");
 
-  const keyRange = IDBKeyRange.lowerBound(0)
-  const cursorRequest = objStore.openCursor(keyRange)
+  const keyRange = IDBKeyRange.lowerBound(0);
+  const cursorRequest = objStore.openCursor(keyRange);
 
-  const todos = []
+  const todos = [];
 
   transaction.oncomplete = function(e) {
     // Execute the callback function.
-    callback(todos)
-  }
+    callback(todos);
+  };
 
   cursorRequest.onsuccess = function(e) {
-    const result = e.target.result
+    const result = e.target.result;
 
     if (!!result === false) {
-      return
+      return;
     }
 
-    todos.push(result.value)
+    todos.push(result.value);
 
-    result.continue()
-  }
+    result.continue();
+  };
 
-  cursorRequest.onerror = onDberror
+  cursorRequest.onerror = onDberror;
 }
 
 /**
@@ -78,53 +78,53 @@ export function fetchTodos(callback) {
  */
 export function createTodo(text, callback) {
   // Get a reference to the db.
-  const db = datastore
+  const db = datastore;
 
   // Initiate a new transaction.
-  const transaction = db.transaction(['todo'], 'readwrite')
+  const transaction = db.transaction(["todo"], "readwrite");
 
   // Get the datastore.
-  const objStore = transaction.objectStore('todo')
+  const objStore = transaction.objectStore("todo");
 
   // Create a timestamp for the todo item.
-  const timestamp = new Date().getTime()
+  const timestamp = new Date().getTime();
 
   // Create an object for the todo item.
   const todo = {
     text: text,
-    timestamp: timestamp,
-  }
+    timestamp: timestamp
+  };
 
   // Create the datastore request.
-  const request = objStore.put(todo)
+  const request = objStore.put(todo);
 
   // Handle a successful datastore put.
   request.onsuccess = function(e) {
     // Execute the callback function.
-    callback(todo)
-  }
+    callback(todo);
+  };
 
   // Handle errors.
-  request.onerror = onDberror
+  request.onerror = onDberror;
 }
 
 /**
  * Delete a todo item.
  */
 export function deleteTodo(id, callback) {
-  const db = datastore
-  const transaction = db.transaction(['todo'], 'readwrite')
-  const objStore = transaction.objectStore('todo')
+  const db = datastore;
+  const transaction = db.transaction(["todo"], "readwrite");
+  const objStore = transaction.objectStore("todo");
 
-  const request = objStore.delete(id)
+  const request = objStore.delete(id);
 
   request.onsuccess = function(e) {
-    callback()
-  }
+    callback();
+  };
 
-  request.onerror = onDberror
+  request.onerror = onDberror;
 }
 
 function onDberror(e) {
-  console.error(e)
+  console.error(e);
 }
--- main.js
+++ main.js
@@ -1,106 +1,102 @@
-import { openDb, fetchTodos, createTodo, deleteTodo } from './db.js'
+import { openDb, fetchTodos, createTodo, deleteTodo } from "./db.js";
 
 window.onload = function() {
   // Display the todo items.
-  openDb(refreshTodos)
+  openDb(refreshTodos);
 
   // Get references to the form elements.
-  const newTodoForm = document.getElementById('new-todo-form')
-  const newTodoInput = document.getElementById('new-todo')
+  const newTodoForm = document.getElementById("new-todo-form");
+  const newTodoInput = document.getElementById("new-todo");
 
   // Handle new todo item form submissions.
   newTodoForm.onsubmit = function() {
     // Get the todo text.
-    const text = newTodoInput.value
+    const text = newTodoInput.value;
 
     // Check to make sure the text is not blank (or just spaces).
-    if (text.replace(/ /g, '') !== '') {
+    if (text.replace(/ /g, "") !== "") {
       // Create the todo item.
       createTodo(text, function(todo) {
-        refreshTodos()
-      })
+        refreshTodos();
+      });
     }
 
     // Reset the input field.
-    newTodoInput.value = ''
+    newTodoInput.value = "";
 
     // Don't send the form.
-    return false
-  }
-}
+    return false;
+  };
+};
 
 // Update the list of todo items.
 function refreshTodos() {
   fetchTodos(function(todos) {
-    const todoList = document.getElementById('todo-items')
-    todoList.innerHTML = ''
+    const todoList = document.getElementById("todo-items");
+    todoList.innerHTML = "";
 
     for (let index = 0; index < todos.length; index++) {
       // Read the todo items backwards (most recent first).
-      const todo = todos[todos.length - 1 - index]
+      const todo = todos[todos.length - 1 - index];
 
-      const li = document.createElement('li')
-      li.id = 'todo-' + todo.timestamp
-      const checkbox = document.createElement('input')
-      checkbox.type = 'checkbox'
-      checkbox.className = 'todo-checkbox'
-      checkbox.setAttribute('data-id', todo.timestamp)
+      const li = document.createElement("li");
+      li.id = "todo-" + todo.timestamp;
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.className = "todo-checkbox";
+      checkbox.setAttribute("data-id", todo.timestamp);
 
-      li.appendChild(checkbox)
+      li.appendChild(checkbox);
 
-      const span = document.createElement('span')
-      span.innerHTML = todo.text
+      const span = document.createElement("span");
+      span.innerHTML = todo.text;
 
-      li.appendChild(span)
+      li.appendChild(span);
 
-      
-      
-      const editBtn = document.createElement('input');
-      editBtn.type = 'button';
-      editBtn.className = 'editBtn';
-      editBtn.value = 'Edit';
-      editBtn.setAttribute('edit-id', todo.timestamp);
+      const editBtn = document.createElement("input");
+      editBtn.type = "button";
+      editBtn.className = "editBtn";
+      editBtn.value = "Edit";
+      editBtn.setAttribute("edit-id", todo.timestamp);
 
-      const saveBtn = document.createElement('input');
-      saveBtn.type = 'button';
-      saveBtn.className = 'saveBtn';
-      saveBtn.value = 'Save';
-      saveBtn.setAttribute('save-id', todo.timestamp);
+      const saveBtn = document.createElement("input");
+      saveBtn.type = "button";
+      saveBtn.className = "saveBtn";
+      saveBtn.value = "Save";
+      saveBtn.setAttribute("save-id", todo.timestamp);
 
       li.appendChild(editBtn);
 
-      todoList.appendChild(li)
+      todoList.appendChild(li);
 
       // Setup an event listener for the checkbox.
-      checkbox.addEventListener('click', function(e) {
-        const id = parseInt(e.target.getAttribute('data-id'))
+      checkbox.addEventListener("click", function(e) {
+        const id = parseInt(e.target.getAttribute("data-id"));
 
-        deleteTodo(id, refreshTodos)
-      })
+        deleteTodo(id, refreshTodos);
+      });
 
-      editBtn.addEventListener('click', function(e) {
-        const idEdit = parseInt(e.target.getAttribute('edit-id'))
+      editBtn.addEventListener("click", function(e) {
+        const idEdit = parseInt(e.target.getAttribute("edit-id"));
 
-
-        const editField = document.createElement('input');
+        const editField = document.createElement("input");
         editField.value = todo.text;
 
         li.replaceChild(editField, span);
         li.replaceChild(saveBtn, editBtn);
 
-        saveBtn.addEventListener('click', function(e) {
+        saveBtn.addEventListener("click", function(e) {
           const editText = editField.value;
-          if (editText.replace(/ /g, '') !== '') {
+          if (editText.replace(/ /g, "") !== "") {
             // Create the todo item.
             createTodo(editText, function(todo) {
-              deleteTodo(idEdit, refreshTodos)
-          })
-        }
-        else {
-          deleteTodo(idEdit, refreshTodos);
-        }
-      })
-      })
+              deleteTodo(idEdit, refreshTodos);
+            });
+          } else {
+            deleteTodo(idEdit, refreshTodos);
+          }
+        });
+      });
     }
-  })
+  });
 }
